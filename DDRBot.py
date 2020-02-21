cc800d348809a86e909928f940bb4ec8d9897b9a
CyberKitsune <920550+cyberkitsune@users.noreply.github.com>
2020-02-21 11:10:56 -0800

    what is this ORM

--- DDRBot.py
+++ DDRBot.py
@@ -700,9 +700,9 @@ class DDRBotClient(discord.Client):
             # Check user
             query = User.select().where(id == int(item[0]))
             if not query.exists():
-                u = User.get_or_create(id=int(item[0]), display_name=self.get_user(item[0]).name)
-            else:
-                u = User.get_by_id(int(item[0]))
+                User.get_or_create(id=int(item[0]), display_name=self.get_user(item[0]).name)
+
+            u = User.get_by_id(int(item[0]))
 
             from DDRGenie.DDRDataTypes import DDRScreenshot, DDRParsedData
             from PIL import Image
